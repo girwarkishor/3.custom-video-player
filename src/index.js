e0843b4fbf89aa0a9986572de0be9f7311689504
girwarkishor <girwar.kishor@gmail.com>
2020-07-24 23:19:35 +0530

    Video Progress Bar & TimeStamp

--- src/index.js
+++ src/index.js
@@ -26,12 +26,27 @@ function updatePlayIcon() {
 
 // Update progress & timestamp
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    // format 00
+    mins = "0" + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
@@ -44,7 +59,7 @@ function stopVideo() {
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
-video.addEventListener("click", updateProgress);
+video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 

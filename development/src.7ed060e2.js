ceb920041270ed5b0c277b648d21fffdc8f918bf
girwarkishor <girwar.kishor@gmail.com>
2020-07-24 23:01:13 +0530

    Play, Pause & Stop

--- development/src.7ed060e2.js
+++ development/src.7ed060e2.js
@@ -193,6 +193,54 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./scss/main.scss");
+
+var video = document.getElementById("video");
+var play = document.getElementById("play");
+var stop = document.getElementById("stop");
+var progress = document.getElementById("progress");
+var timestamp = document.getElementById("timestamp"); // Play & Pause video
+
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+} // Update play/pause icon
+
+
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+} // Update progress & timestamp
+
+
+function updateProgress() {
+  return true;
+} // Set video time to progress
+
+
+function setVideoProgress() {
+  return true;
+} // Stop video
+
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+} // Event listeners
+
+
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("click", updateProgress);
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
 },{"./scss/main.scss":"../src/scss/main.scss"}],"C:/Users/girwa/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 import "./scss/main.scss";
+
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Play & Pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+// Update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+// Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+// Event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("click", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
